25776e4fc950d4d85e23a300cbc536a837fd0565
JoseLucasapp <josluc2016@gmail.com>
2021-03-30 11:20:00 -0300

    update

--- public/script/app.js
+++ public/script/app.js
@@ -1,25 +1,66 @@
-const incomeBtn = document.getElementById('incomeBtn');
-const expenseBtn = document.getElementById('expenseBtn');
-const incomeTextInput = document.getElementById('incomeTextInput');
-const incomeInput = document.getElementById('incomeInput');
-const expenseTextInput = document.getElementById('expenseTextInput');
-const expenseInput = document.getElementById('expenseInput');
+const incomeBtn = document.getElementById('btnNewIncome');
+const expenseBtn = document.getElementById('btnNewExpense');
+const mainBtn = document.getElementById('mainBtn');
+const actionResults = document.getElementById('actionResults');
+const actionName = document.getElementById('name');
+const actionValue = document.getElementById('value');
 const incomeForm = document.getElementById('incomeForm');
-const expenseForm = document.getElementById('expenseForm');
+const yourWallet = document.getElementById('wallet');
+const allLists = document.getElementById('allList');
+const allIncomesList = document.getElementById('allIncomesList');
+const allExpensesList = document.getElementById('allExpensesList');
 
-incomeBtn.addEventListener('click',()=>{
-    document.getElementById('incomeAction').style.display = 'block';
-    document.getElementById('expenseAction').style.display = 'none';
-});
-expenseBtn.addEventListener('click',()=>{
-    document.getElementById('incomeAction').style.display = 'none';
-    document.getElementById('expenseAction').style.display = 'block';
+let wallet = 0;
+let visible = true;
+
+mainBtn.addEventListener('click',()=>{
+    if(actionResults.style.display === 'block'){
+        actionResults.style.display = 'none';
+    }else{
+        actionResults.style.display = 'block';
+    }
 });
 
-incomeForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    console.log(e)
+yourWallet.addEventListener('click',()=>{
+    if(visible === true){
+        yourWallet.innerHTML = '$ -,--';
+        visible = false;
+    }else{
+        visible = true;
+        yourWallet.innerHTML = `$ ${wallet.toFixed(2)}`
+    }
+})
+
+incomeBtn.addEventListener('click',(e)=>{
+    let idColor = Date.now();
+    wallet += parseFloat(actionValue.value);
+    if(wallet < 0){
+        yourWallet.style.color = 'red';
+    }else{
+        yourWallet.style.color = 'black';
+    }
+    allLists.innerHTML += `<li id=${idColor}>${actionName.value} $${actionValue.value}</li>`
+    document.getElementById(idColor).style.color = 'green';
+    allIncomesList.innerHTML += `<li>${actionName.value} $${actionValue.value}</li>`
+    yourWallet.innerText = `$ ${wallet.toFixed(2)}`;
+    actionName.value = '';
+    actionValue.value = 0;
+    actionResults.style.display = 'none';
 });
-expenseForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
+
+expenseBtn.addEventListener('click',(e)=>{
+    let idColor = Date.now();
+    wallet -= parseFloat(actionValue.value);
+    if(wallet < 0){
+        yourWallet.style.color = 'red';
+    }else{
+        yourWallet.style.color = 'black';
+    }
+    allLists.innerHTML += `<li id=${idColor}>${actionName.value} $${actionValue.value}</li>`
+    document.getElementById(idColor).style.color = 'red';
+    allExpensesList.innerHTML += `<li>${actionName.value} $${actionValue.value}</li>`
+    yourWallet.innerText = `$ ${wallet.toFixed(2)}`;
+    actionName.value = '';
+    actionValue.value = 0;
+    actionResults.style.display = 'none';
 })
